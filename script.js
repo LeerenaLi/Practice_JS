1b89aceb48ba505daec9c5464ee27900e1e46b5d
Leerena <leerena7@gmail.com>
2022-03-18 18:12:52 +0300

    added new functions start and checkSavings

--- script.js
+++ script.js
@@ -39,48 +39,60 @@
 
     Чтобы получить от пользователя не стороку, а числовой тип данных ставим перед prompt унарный плюс:
     +prompt
-
-
-    Вместо for другие циклы:
-
-    Используем цикл WHILE
-
-    let i = 0;
-    while (i < 2) {
-        let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-             b = prompt ("Во сколько обойдется?", "");
-
-        if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-            console.log ("done");
-            appData.expenses[a] = b;
-        } else {
-            console.log ("bad result");
-            i--;
-        } i++;     
-    };
-
-
-
-    Используем цикл DO...WHILE
-
-    let i = 0;
-    do {
-        let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-            b = prompt ("Во сколько обойдется?", "");
-
-        if( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50){
-            console.log ("done");
-            appData.expenses[a] = b;
-        } else {
-            console.log ("bad result");
-            i--;
-        }
-        i++;
-        } while(i < 2);
+ 
 */
 
-let money = +prompt("Ваш бюджет на месяц?", '');
-let time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+
+/* 
+    Функция start - чтобы вопросы не выводились просто так,
+    внутрь помещаем переменные money и time, 
+    но чтобы к ним можно было обратиться из любого места, выводим их также на глобальный уровень/
+
+    Также в функцию добавляем проверку, чтобы человек не мог оставить строку пустой,
+    ввести не числа или отменить prompt. Используем цикл while
+
+    Для проверки переменной money на числа используем команду isNaN
+
+    isNaN - возвращает true, когда введены НЕ числа
+    || money == "" или пустая строка
+    || money == null или пользователь нажал кнопку отмена
+
+    Если пользователь ввел не числа, пустую строку или нажал отмену - задаем вопрос еще раз,
+    т.е. цикл продолжается
+
+    start(); - вызываем функцию
+
+    Выбор обязательных расходов, цикл for мы также оформим в виде функции chooseExpenses
+    и вызовем ее
+
+    Метод toFixed() - округляет число. При пустых скобках - до целого,
+    (1) - до первого знака после запятой и т.д.
+    Но он меняет тип данных - возвращает строковое значение
+
+    задание:
+    Создать функцию для определения необязательных расходов (chooseOptExpenses):
+    - Необходимо 3 раза спросить у пользователя “Статья необязательных расходов?”
+    - Записать ответы в объект optionalExpenses в формате Номер - Ответ
+    optionalExpenses: {
+    1 : “ответ на вопрос”
+    }
+
+*/
+
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", '');
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+    while(isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", '');
+    }
+};
+start();
+
+
 
 let appData = {
     budget: money,
@@ -88,36 +100,73 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
-for(i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-        b = prompt("Во сколько обойдется?", '');
-
-    if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-        && a != '' && b != '' && a.length < 50){
-        console.log("done");    
-        appData.expenses[a] = b;
-    } else {
-        console.log("bad result");
-        i--;
+function chooseExpenses() {
+    for(i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = prompt("Во сколько обойдется?", '');
+    
+        if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+            && a != '' && b != '' && a.length < 50){
+            console.log("done");    
+            appData.expenses[a] = b;
+        } else {
+            console.log("bad result");
+            i--;
+        };
+        
     };
-    
 };
+chooseExpenses();
 
-appData.moneyPerDay = appData.budget / 30;
-    
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-if(appData.moneyPerDay < 100) {
-    console.log("Минимальный уровень достатка");
-}else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Средний уровень достатка");
-} else if(appData.moneyPerDay > 2000) {
-    console.log("Высокий уровень достатка");
-} else {
-    console.log("Произошла ошибка");
+function chooseOptExpenses() {
+    for(i = 0; i < 3; i++) {
+        let questionOptExpenses = prompt("Статья необязательных расходов?");
+        appData.optionalExpenses[i + 1] = questionOptExpenses;
+        console.log(appData.optionalExpenses);
+    };
 };
+chooseOptExpenses();
 
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert("Ежедневный бюджет: " + appData.moneyPerDay);
+};
+detectDayBudget();
+
+function detectLevel(){
+    if(appData.moneyPerDay < 100) {
+        console.log("Минимальный уровень достатка");
+    }else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Средний уровень достатка");
+    } else if(appData.moneyPerDay > 2000) {
+        console.log("Высокий уровень достатка");
+    } else {
+        console.log("Произошла ошибка");
+    };    
+}; 
+detectLevel();
+
+
+/* Расчет накоплений с депозита
+
+    appData.savings == true - если накопления есть, т.е. возвращет true?
+    то задаем вопросы пользователю. Перед prompt + - чтобы вводили числа
+
+    appData.monthIncome - обращаемся к объекту и создаем новое свойство (приход в мес)
+
+*/
+
+function checkSavings(){
+    if(appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений"),
+            percent = +prompt("Под какой процент?");
+
+        appData.monthIncome = save/100/12*percent;
+        alert("Доход в месяц с депозита: " + appData.monthIncome);
+    }
+};
+checkSavings();
 
